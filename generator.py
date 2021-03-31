11219acd7a169001e940e20c530f342cca882852
znjuko <znajderko@yandex.ru>
2021-03-31 22:56:24 +0300

    small fix

--- generator.py
+++ generator.py
@@ -2,12 +2,12 @@ import sys
 from random import randrange
 
 filename = sys.argv[1]
-number_count = 4000000
+number_count = 1000
 
 f = open(filename, "w+")
 
 for i in range(number_count):
-    f.write(str(randrange(3000))+"\n")
+    f.write(str(randrange(200))+"\n")
 
 f.close()
 
