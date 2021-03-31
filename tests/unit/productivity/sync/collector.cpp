f845603777456fd2c24e4768807accd9c6443ce9
znjuko <znajderko@yandex.ru>
2021-03-31 23:18:28 +0300

    ...

--- tests/unit/productivity/sync/collector.cpp
+++ tests/unit/productivity/sync/collector.cpp
@@ -14,11 +14,11 @@ extern "C" {
 const char* open_params = "r";
 const char* test_filename = "data/file.txt";
 const size_t point_size = 4000;
-const size_t test_count = 5;
+const size_t test_count = 40;
 
 TEST(COLLECTOR, COUNT_TIME_SYNC)
 {
-    auto start = steady_clock::now();
+    clock_t begin = clock();
 
     for (size_t i = 0; i < test_count; ++i) {
         File* reader = create_file_reader(test_filename, open_params);
@@ -28,8 +28,5 @@ TEST(COLLECTOR, COUNT_TIME_SYNC)
         destroy_file_reader(&reader);
     }
 
-    auto end = steady_clock::now();
-    auto elapsed = duration_cast<milliseconds>(end - start);
-
-    std::cout << "time elapsed: " << elapsed.count() / test_count << std::endl;
+    std::cout << "time elapsed: " << (double)(clock() - begin) / (CLOCKS_PER_SEC * test_count) << std::endl;
 }
